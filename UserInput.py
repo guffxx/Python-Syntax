a7c09caa22c4e29667a6cf393af14936773377cc
David <161796631+guffxx@users.noreply.github.com>
2026-04-04 00:24:48 -0700

    practice

--- UserInput.py
+++ UserInput.py
@@ -1,9 +1,10 @@
-
 # User Input
-# The input() function requires a prompt message, which it will print out 
-# for the user before they enter the new information. 
-# For example: 
+# The input() function requires a prompt message, which it will print out
+# for the user before they enter the new information.
+# For example:
 
 likes_snakes = input("Do you like snakes? ")
 
-favourite_apple = input("What is your fave type of Apple? ") #dont forget the SPACE
+favourite_apple = input("What is your fave type of Apple? ")  # dont forget the SPACE
+
+print("Your favourite apple is a " + favourite_apple + ".")
--- test.py
+++ test.py
@@ -1,15 +1,23 @@
-coins = 100
-bills = 150
+while True:
+    coins_input = input("How many coins do you have? ")
+    bills_input = input("How many bills do you have? ")
+    if coins_input.isdigit() and bills_input.isdigit():
+        coins = int(coins_input)
+        bills = int(bills_input)
+        break
+    else:
+        print("Invalid input. Please enter a valid number.")
+        continue
 
 
-def calculate_total(coins, bills):
-    total = coins + bills
-    return total
+print(coins)
+print(bills)
 
 
-myTotal = calculate_total(coins, bills)
+myTotal = coins + bills
 
-if myTotal >= 200:
-    print(f"Total is over 200: {myTotal}")
-else:
-    print(f"Total is under 200: {myTotal}")
+match myTotal:
+    case n if n > 100:
+        print(f"Total is over 100: {myTotal}")
+    case n if n < 100:
+        print(f"Total is below 100: {myTotal}")
