5b02d0d2d7e48fd2701cc52170184c8b65d7364d
David <161796631+iamcrefusion@users.noreply.github.com>
2026-04-07 12:09:14 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 import random
 import time
 
-num = random.randint(1, 100)
-print("You have asked for a random number between 1 and 100.")
-time.sleep(2)
-print("Standby...")
-time.sleep(2)
-print("...")
-time.sleep(1)
+
+def random_num_gen():
+    return random.randint(1, 100)
+
+
+num = random_num_gen()
 print("Your number is:", num)
